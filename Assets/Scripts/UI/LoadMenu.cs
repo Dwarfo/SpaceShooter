d5975ecd7142829c4085ef177f2a40fea23e7376
DESKTOP-JR6KDJO\hurma <hurma2rf@gmail.com>
2019-01-10 11:18:05 +0100

    Initial
    Game with a full gameloop, weapon system ai constructor and level transition.

--- Assets/Scripts/UI/LoadMenu.cs
+++ Assets/Scripts/UI/LoadMenu.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
 public class LoadMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Button backButton;
+	
+
+	void Start ()
+    {
+        var info = new DirectoryInfo(Application.persistentDataPath);
+        var fileInfo = info.GetFiles();
+        int fileNum = fileInfo.Length;
+
+        foreach (FileInfo file in fileInfo)
+        {
+            string nameString = file.ToString();
+            int index = nameString.IndexOf(Application.persistentDataPath);
+            string cleanPath = (index < 0) ? nameString : nameString.Remove(index, Application.persistentDataPath.Length);
+
+        }
+
 	
 	// Update is called once per frame
 	void Update () {
